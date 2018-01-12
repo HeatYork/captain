8a66033d3b6d8c9f4a7d0cabd109c96dd2a1b8e6
Heat Lai <hitgunlai@gmail.com>
2018-01-12 16:26:45 +0800

    change: index page

--- captain/Application/Test.php
+++ captain/Application/Test.php
@@ -4,12 +4,21 @@ namespace Application;
 
 class Test extends \Template\AMaster
 {
+    public function index()
+    {
+        $this->setTitle('首頁');
+        $data = array(
+            'test' => '首頁內容'
+        );
+
+        $this->display('test.php', $data);
+    }
+
     public function echoTest()
     {
         $this->setTitle('我愛的人');
         $test = array('test'=>'雞龜骨滾羹');
 
         $this->display('test.php', $test);
-
     }
 }
--- captain/Config/RoutingTable.php
+++ captain/Config/RoutingTable.php
@@ -7,6 +7,7 @@ class RoutingTable
     public static function getRoutingTable() : array
     {
         return array(
+            "" => array("\Application\Test", "index"),
             "test/echotest" => array("\Application\Test", "echoTest")
         );
     }
